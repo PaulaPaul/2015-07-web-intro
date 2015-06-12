628fac46920c0f79e0d49bc55cce7173df795fa5
PaulaPaul <paula.paul@paulapaul.com>
2015-06-12 19:37:59 +0000

    updates after trial run

--- MyThings/config/routes.rb
+++ MyThings/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'pages/home'
 
   get 'pages/about'
-
-  resources :things
   root to: 'things#index'
+  resources :things
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
